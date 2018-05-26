ad5fdfdc5b4476cb6bf645f54209243261918b8d
NutchapolBook <nutchapol.p@ku.th>
2018-05-27 02:06:02 +0700

    [Edit]fix bg

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,12 @@
 @section('home')
 
 <div style="width:768px; margin:0 auto;" class="col-sm-12 blog-main" >
-    <div class="w3-container row" style="bottom: 0;">
+    <div class="w3-container row" style="bottom:0; ">
         <picture>
-            <source media="(min-width: 1441px)" srcset="{{asset('images/bg.jpg')}}">
-            <source media="(max-width: 400px)" srcset="{{asset('images/bg-mobile2.jpg')}}">
+            <source media="(min-width: 1441px)" srcset="{{asset('images/bg-xl.jpg')}}">
+            <source media="(min-width: 1024px)" srcset="{{asset('images/bg-l.jpg')}}">
+            <source media="(min-width: 560px)" srcset="{{asset('images/bg.jpg')}}">
+            <source media="(max-width: 400px)" srcset="{{asset('images/bg-mobile.jpg')}}">
             <img src="{{asset('images/bg.jpg')}}" alt="background" style="width:100%; height:100%;">
         </picture>
         <div class="text-block">
